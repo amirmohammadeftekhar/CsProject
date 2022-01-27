dc1ce5ad0ffb86d8fe55054556b8c6224f8dc1a9
amirmohammad eftekhar <amirmohammadeftekhar@gmail.com>
2022-01-27 13:07:02 +0330

    Remove whitespaces

--- 4-calculator.asm
+++ 4-calculator.asm
@@ -5,15 +5,15 @@
     return_message db 10, 13, 100 dup(0),'$'
 
 .STACK
-    stack_array dw 100h dup(0)    
-                           
+    stack_array dw 100h dup(0)
+
 
 .CODE
 
     get_char PROC ; inputs a char in al; changes ax
         mov ah,1
         int 21h
-        ret 
+        ret
     get_char ENDP
 
     puts PROC ; outputs the string in [dx]
@@ -36,28 +36,28 @@
         END_LOOP_GET_STRING:
         pop ax
         ret
-    get_string ENDP    
+    get_string ENDP
 
     MAIN PROC
         mov ax,DATA
         mov ds,ax
-        
+
         mov ax,STACK
         mov ss,ax
-        
+
         lea sp,stack_array + 200h
 
         lea dx,return_message + 2
         mov di,dx
-        call get_string        
-        
+        call get_string
+
         lea dx,return_message
         call puts
-    
-        EXIT:    
+
+        EXIT:
             mov ah,4ch
             int 21h
-    
+
     MAIN ENDP
 
 
