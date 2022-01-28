7613a61704e627b186f791f909f4f53de9735276
shayan <shayanpardis82@gmail.com>
2022-01-28 08:32:10 +0330

    add 4-calculator.cpp  and complete initial version of 4-calculator (without error handling and output

--- 4-calculator.asm
+++ 4-calculator.asm
@@ -1,14 +1,51 @@
-.MODEL SMALL
+
 
 .DATA
     welcome_message db 'Enter a string to evaluate!', 10, '$'
-    return_message db 10, 13, 100 dup(0),'$'
-
+    return_message db 10, 13, 'Result =                 $' 
+    base dw 10
+    ; cr is stored in al
+    ; answer is stored in dx
+    ; bx is used as temp
 .STACK
     stack_array dw 100h dup(0)    
                            
 
 .CODE
+    SPACE EQU 32
+    ENTER EQU 13
+    P_OPEN EQU 40
+    P_CLOSE EQU 41
+    ZERO EQU 48
+    PLUS EQU 43
+    MINUS EQU 45
+    STAR EQU 42
+    SLASH EQU 47
+
+    al_to_int MACRO label_to_escape
+        mov ah, al
+        sub ah, ZERO
+        jl label_to_escape
+        cmp ah, 9
+        jg label_to_escape   
+    ENDM
+
+    multiply_word_integer MACRO b, overflow_label ; ax * b
+        ; lets assume that we do not encounter overflow for now
+        push dx
+        imul b
+        jo overflow_label
+        pop dx
+    ENDM
+
+    divide_word_integer MACRO b, overflow_label ; ax * b
+        ; lets assume that we do not encounter overflow for now
+        push dx
+        xor dx, dx
+        idiv b
+        jo overflow_label
+        pop dx
+    ENDM
 
     get_char PROC ; inputs a char in al; changes ax
         mov ah,1
@@ -38,6 +75,137 @@
         ret
     get_string ENDP    
 
+    read_blank PROC
+        LOOP_READ_BLANK:
+        call get_char
+        cmp al, SPACE
+        je LOOP_READ_BLANK
+        ret
+    read_blank ENDP    
+
+    read_number PROC ; returns in ax
+        push bx
+
+        mov bx, 0
+
+        LOOP_READ_NUMBER:
+        cmp al, SPACE
+        je REMOVE_SPACES
+        al_to_int END_LOOP_READ_NUMBER
+        xchg ax,bx
+        multiply_word_integer base, EXIT_OVERFLOW
+        mov bl, 0
+        xchg bl,bh
+        add ax, bx
+        jo EXIT_OVERFLOW
+        xchg ax, bx
+        add al, ZERO
+        
+        call get_char
+        jmp LOOP_READ_NUMBER
+        REMOVE_SPACES:
+        call read_blank
+        END_LOOP_READ_NUMBER:
+
+        mov dx, bx
+        pop bx
+        ret
+    read_number ENDP
+
+    read_cr PROC ; returns in ax
+        cmp al, P_OPEN
+        jne END_IF_1_READ_CR
+        call read_blank
+        call read_expr
+        cmp al, P_CLOSE
+        jne EXIT_NVALID
+        call read_blank
+        ret
+        END_IF_1_READ_CR:
+        al_to_int END_IF_2_READ_CR
+        call read_number
+        ret
+        END_IF_2_READ_CR:        
+        jmp EXIT_NVALID
+        ret
+    read_cr ENDP
+
+    read_md PROC
+        push bx
+
+        call read_cr
+        mov bx, dx
+
+        LOOP_READ_MD:
+
+        cmp al, STAR 
+        jne END_STAR
+        call read_blank
+        call read_cr
+        xchg dx, ax
+        xchg ax,bx
+        multiply_word_integer bx, EXIT_OVERFLOW
+        xchg ax,bx
+        xchg dx, ax
+        jmp LOOP_READ_MD
+
+        END_STAR:   
+        cmp al, SLASH     
+        jne END_SLASH
+        call read_blank
+        call read_cr
+        xchg dx, ax
+        xchg ax,bx
+        divide_word_integer bx, EXIT_OVERFLOW
+        xchg ax,bx
+        xchg dx, ax
+        jmp LOOP_READ_MD
+        END_SLASH:
+
+        mov dx, bx
+        pop bx
+        ret
+    read_md ENDP
+
+    PROC read_expr
+        push bx
+
+        call read_md
+        mov bx, dx
+
+        LOOP_READ_EXPR:
+
+        cmp al, PLUS 
+        jne END_PLUS
+        call read_blank
+        call read_expr
+        xchg dx, ax
+        xchg ax,bx
+        add ax, bx
+        jo EXIT_OVERFLOW
+        xchg ax,bx
+        xchg dx, ax
+        jmp LOOP_READ_EXPR
+
+        END_PLUS:
+        cmp al, MINUS        
+        jne END_MINUS
+        call read_blank
+        call read_expr
+        xchg dx, ax
+        xchg ax,bx
+        sub ax, bx
+        jo EXIT_OVERFLOW
+        xchg ax,bx
+        xchg dx, ax
+        jmp LOOP_READ_EXPR
+        END_MINUS:
+
+        mov dx, bx
+        pop bx
+        ret
+    read_expr ENDP
+    
     MAIN PROC
         mov ax,DATA
         mov ds,ax
@@ -47,13 +215,25 @@
         
         lea sp,stack_array + 200h
 
-        lea dx,return_message + 2
-        mov di,dx
-        call get_string        
+
+        call read_blank
+        call read_expr
+        cmp al,ENTER
+        jne EXIT_NVALID
+        jmp EXIT
+        ; lea dx,return_message + 2
+        ; mov di,dx
+        ; call get_string        
         
-        lea dx,return_message
-        call puts
+        ; lea dx,return_message
+        ; call puts
     
+        EXIT_OVERFLOW:
+        jmp EXIT
+
+        EXIT_NVALID:
+        jmp EXIT
+
         EXIT:    
             mov ah,4ch
             int 21h
---
+++ 4-calculator.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//--------- Rules -------------
+// E: Any expression
+// MD: Expression that is not splitted by + or -
+// CR: Expression that is not splitted at all
+// N : number
+// E -> MD + E
+// E -> MD - E
+// E -> MD
+// MD -> MD * CR
+// MD -> MD / CR
+// MD -> CR
+// CR -> (CR)
+// CR -> N
+// N -> [0-9]
+// N -> [0-9]N
+//---------------------------
+
+// this does not catch overflow
+
+char cr;
+
+void get() {
+    do {
+        cr = getchar();
+    } while(cr == ' ');
+}
+
+
+int read_expr();
+int read_md();
+int read_number();
+int read_cr();
+
+int read_number() {
+    int x = 0;
+    while('0' <= cr && cr <= '9') {
+        x = 10 * x + (cr - '0');
+        cr = getchar();        
+    }
+    if(cr == ' ')
+        get();
+    return x;
+}
+int read_cr() {
+    if(cr == '(') {
+        get();
+        int x = read_expr();
+        if(cr != ')')
+            throw "invalid expression";
+        get();
+        return x;
+    } else if('0' <= cr && cr <= '9') {
+        return read_number();
+    }
+    throw "invalid expression";
+}
+int read_md() {
+    int x = read_cr();
+    while(cr == '*' || cr == '/') {
+        if(cr == '*') {
+            get();
+            x = x * read_cr();
+        } else if(cr == '/') {
+            get();
+            x = x / read_cr();
+        }
+    }
+    return x;
+}
+int read_expr() {
+    int x = read_md();
+    if(cr == '+') {
+        get();
+        return x + read_expr();
+    }
+    if(cr == '-') {
+        get();
+        return x - read_expr();
+    }
+    return x;
+}
+
+
+int main() {
+    cout << int('/') << endl;
+    try {
+        get();
+        cout << read_expr() << "\n";
+        if(cr != '\n')
+            throw "expression is not valid";
+    } catch(string s) {
+        cout << s << "\n";
+    }
+    return 0;
+}
