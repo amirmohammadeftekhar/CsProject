6aebad6ca9272d4ad23078e59aeef89de6cf6945
Kalam132 <alipasha.montaseri@gmail.com>
2022-01-29 17:54:00 +0330

    11_8086 implemented

--- 11_8086.asm
+++ 11_8086.asm
@@ -2,12 +2,12 @@ include 'emu8086.inc'
 
 org 100h      
 lea di,s
-mov dx,200
+mov dx,100
 call get_string
 
 call pthis
 db 13,10,0
-
+; odd start
 mov cx,0
 
 lop:
@@ -23,21 +23,25 @@ lop:
     sub di,bx   
     mov dh,s[di]
     
+    cmp cx,bx
+    jl pass_
+    
     cmp dh,dl
-    je chk
-  ;dec bx
+    jz chk
+  pass_:
+  dec bx
   
-  mov ax,bx
-  call print_num
-  call pthis
-  db 13,10,0
+  ;mov ax,bx
+  ;call print_num
+  ;call pthis
+  ;db 13,10,0
   
-  mov ax,bx
-  add ax,bx
-  inc ax
+  mov al,bl
+  add al,bl
+  inc al
   mov di,cx
   cmp sz,al
-  jp pass
+  jg pass
   
   mov sz,al
   mov le,cl
@@ -51,7 +55,63 @@ lop:
   mov dl,s[di]
   cmp dl,0
   jne lop
+; odd end
+; even start
+mov cx,0
 
+lop2:
+  mov bh,0
+  mov bl,-1
+  chk2:
+    inc bl
+    mov di,cx
+    add di,bx
+    add di,1
+    mov dl,s[di]
+    
+    mov di,cx
+    sub di,bx   
+    mov dh,s[di]
+    
+    cmp cx,bx
+    jl pass_2
+    
+    cmp dh,dl
+    jz chk2
+  pass_2:
+  dec bx
+  
+  ;mov ax,bx
+  ;call print_num
+  ;call pthis
+  ;db 13,10,0
+  
+  mov al,bl
+  add al,bl
+  add al,2
+  mov di,cx
+  cmp sz,al
+  jg pass2
+  
+  mov sz,al
+  mov le,cl
+  sub le,bl
+  mov ri,cl
+  add ri,bl
+  add ri,1
+  pass2:  
+  inc cx
+  mov di,cx
+  add di,1
+  mov dl,s[di]
+  cmp dl,0
+  jne lop2
+
+;mov ax,0
+;mov al,sz
+;call print_num   
+;call pthis
+;db 13,10,0
 mov ax,0
 mov al,le
 call print_num   
@@ -64,12 +124,12 @@ db 13,10,0
     
                
 ret
-s db 200dup(0)        
+        
 n db 0
 le db 0
 ri db 0
 sz db 0
-
+s db 100dup(0)
 
 DEFINE_SCAN_NUM; get number in cx
 DEFINE_PRINT_NUM; print number in ax
